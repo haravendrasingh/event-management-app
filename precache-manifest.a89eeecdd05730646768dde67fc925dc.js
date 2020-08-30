c58ee962612a40f7067e14cb80c766a2c71df5f2
haravendrasingh <haravendra.singh@paytm.com>
2020-08-30 18:53:10 +0530

    Updates

---
+++ precache-manifest.a89eeecdd05730646768dde67fc925dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7c77f8bc7be94ee4271cc26c52a63f",
+    "url": "/event-management-app/index.html"
+  },
+  {
+    "revision": "0c87fc9762c412407f53",
+    "url": "/event-management-app/static/css/main.67e08031.chunk.css"
+  },
+  {
+    "revision": "6cb91b1938ef162eb204",
+    "url": "/event-management-app/static/js/2.b366dd7e.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/event-management-app/static/js/2.b366dd7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c87fc9762c412407f53",
+    "url": "/event-management-app/static/js/main.ba1c7265.chunk.js"
+  },
+  {
+    "revision": "83816cd84b03dd3a3976",
+    "url": "/event-management-app/static/js/runtime-main.78ee1974.js"
+  }
+]);
